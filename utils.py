99280ded7fbe334eade7f9dd8bfa5dc39a1ce1c9
Andrew Jordan (Aj) Siciliano <ajs550@miami.edu>
2024-05-24 18:58:29 -0400

    Add files via upload

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ import math
 import string
 import collections
 import pandas as pd
+import numpy as np
 from tqdm import tqdm
 
 from init import *
@@ -26,9 +27,10 @@ matplotlib.rcParams['font.sans-serif'] = ["Arial"]
 def line_break(n=15): print("\n"+"*"*n+"\n")
 
 ###################################
+# clow = (53/255, 229/255, 163/255) #(128.0/255,0.0,0.0)
+clow = (52/255, 223/255, 158/255) #(128.0/255,0.0,0.0)
 
-cred = (128.0/255,0.0,0.0)
-cblue = (102.0/255,153.0/255,204.0/255)
+chigh = (90/255, 177/255, 236/255)#(102.0/255,153.0/255,204.0/255)
 
 def casp15_loss_figure(metrics, file_path, score_name, total): 
 
@@ -48,15 +50,19 @@ def casp15_loss_figure(metrics, file_path, score_name, total):
 
     plt.figure(figsize=(25,15))
 
-    plt.barh(y_idx, high_values, color=cblue, linewidth=1.0, edgecolor='k', label="$N\ Loss("+score_name+"\mathrm{-}score)<0.1$")
+    plt.barh(y_idx, high_values, color=chigh, linewidth=1.0, edgecolor='k', label="$N\ Loss("+score_name+"\mathrm{-}score)<0.1$")
     plt.yticks(y_idx, labels, fontsize = 35)
 
-    plt.barh(y_idx, low_values, color=cred, linewidth=1.0, edgecolor='k', label="$N\ Loss("+score_name+"\mathrm{-}score)<0.05$")
+    for i, v in enumerate(high_values): 
+        if v - low_values[i] > 0: 
+            plt.text(low_values[i] + (v - low_values[i])/2, i, str(v - low_values[i]), color="black", fontsize=30, ha='center', va='center')
 
+    plt.barh(y_idx, low_values, color=clow, linewidth=1.0, edgecolor='k', label="$N\ Loss("+score_name+"\mathrm{-}score)<0.05$")
+
+    for i, v in enumerate(low_values): 
+        if v > 0: plt.text(v/2, i, str(v), color="black", fontsize=30, ha='center', va='center')
+    
     plt.xticks(fontsize = 35)
-
-    new_list = range(0, math.ceil(max(high_values))+1)
-    plt.xticks(new_list)
 
     for label in plt.gca().get_yticklabels():
         if label.get_text() in ["REGRESSION-METALAYER","EBM-METALAYER", "EBM-TRANSFORMER", "REGRESSION-TRANSFORMER"]: label.set_fontweight('bold')
@@ -89,9 +95,12 @@ def ndcg_figure(metrics, file_path, score_name, total):
 
     y_idx = list(range(len(labels)))
 
-    plt.barh(y_idx, values, color=cblue, linewidth=1.0, edgecolor='k')
-    plt.yticks(y_idx, labels, fontsize = 35)
+    plt.barh(y_idx, values, color=chigh, linewidth=1.0, edgecolor='k')
 
+    for i, v in enumerate(values): 
+        if round(v,1) > 0: plt.text(v/2, i, str(round(v,2)), color="black", fontsize=30, ha='center', va='center')
+
+    plt.yticks(y_idx, labels, fontsize = 35)
     plt.xticks(fontsize = 35)
 
     for label in plt.gca().get_yticklabels():
@@ -202,6 +211,8 @@ def general_results(model_inits, is_overall_fold):
             group_mappings_new[id] = group_mappings[id]
             eval_groups += [id]
 
+    group_mappings = group_mappings_new 
+
     model_names = []
 
     for model in model_inits: 
@@ -213,8 +224,7 @@ def general_results(model_inits, is_overall_fold):
         model_names += [model[0][:-5]]
 
         eval_groups += [model[0][:-5]]
-                
-        group_mappings = group_mappings_new
+    
         group_mappings[model[0][:-5]] = model[0][:-5]
 
         truth_df = truth_df.merge(pd.read_csv(csv),on="mdl")
@@ -224,6 +234,7 @@ def general_results(model_inits, is_overall_fold):
     aggregated_metrics = {}
 
     target_map = {}
+    target_results = {}
 
     for index, row in tqdm(truth_df.iterrows(),total=len(truth_df),desc="Aggregating Metrics"):
         target = row["trg"]
@@ -243,6 +254,8 @@ def general_results(model_inits, is_overall_fold):
                 "n_trg_chains": row["n_trg_chains"]
             }
         }
+
+        if target not in target_results: target_results[target] = {}
 
         for group_id in eval_groups:
             
@@ -288,12 +301,13 @@ def general_results(model_inits, is_overall_fold):
 
     ###################################
 
+    is_hetero = {}
+
     CASP15_loss = {}
     CASP15_two_chains = {}
     CASP15_more_than_two_chains = {}
 
     for target in tqdm(target_map,desc="Computing CASP Loss"):
-        
         top_reference = {}
 
         for model in target_map[target]:
@@ -307,12 +321,14 @@ def general_results(model_inits, is_overall_fold):
             if group_id not in CASP15_loss: CASP15_loss[group_id] = [0,0]
             if group_id not in CASP15_two_chains: CASP15_two_chains[group_id] = [0,0]
             if group_id not in CASP15_more_than_two_chains: CASP15_more_than_two_chains[group_id] = [0,0]
+            if group_id not in target_results[target]: target_results[target][group_id] = {}
 
             preds = set()
 
             top_assigned = None
 
             diff = None
+            r_diff = None
 
             try: 
                 
@@ -327,9 +343,9 @@ def general_results(model_inits, is_overall_fold):
                 if len(top_assigned[1]) == 1: 
                     pred_met = target_map[target][top_assigned[1][0]]["values"][score_key]
                     diff = abs(top_reference[score_key][0] - pred_met)    
-
+                    r_diff = diff
+                else: r_diff = "DUP"
                     #if target == "T1123o.pdb": 
-
                         # print(target,"|",diff,"|",top_assigned,"|",group_mappings[group_id],">",pred_met)
 
             except: None
@@ -345,6 +361,8 @@ def general_results(model_inits, is_overall_fold):
 
                 check_set = list(check_set)
                 
+                if target not in is_hetero and len(check_set) > 1: is_hetero[target] = check_set[0] > 2
+
                 if diff < 0.1: 
                     CASP15_loss[group_id][0] += 1
                     if len(check_set) != 1: continue
@@ -356,6 +374,8 @@ def general_results(model_inits, is_overall_fold):
                     if len(check_set) != 1: continue
                     elif check_set[0] == 2: CASP15_two_chains[group_id][1] += 1
                     elif check_set[0] > 2: CASP15_more_than_two_chains[group_id][1] += 1
+                
+            target_results[target][group_id]["CASP Loss"] = r_diff
 
     ###################################
 
@@ -384,7 +404,6 @@ def general_results(model_inits, is_overall_fold):
             rel_i = 1 - (reference_value - model_value)**2
 
             if group_id is not None: 
-
                 if aggregated_metrics[group_id][target][model] in duplicate_values: continue
 
             score += rel_i / math.log2(1 + position)
@@ -414,7 +433,9 @@ def general_results(model_inits, is_overall_fold):
 
                 NDCG[group_id] += group_ranking_score/true_ranking_score
 
-            except: None
+                target_results[target][group_id]["NDCG@3"] = group_ranking_score/true_ranking_score
+
+            except: target_results[target][group_id]["NDCG@3"] = None
 
     ###################################
 
@@ -432,6 +453,63 @@ def general_results(model_inits, is_overall_fold):
         "CASP15_two_chains": CASP15_two_chains,
         "CASP15_more_than_two_chains": CASP15_more_than_two_chains,
         "NDCG": NDCG
-    }
+    }, target_results
 
+###################################
+
+def generate_target_xlsx(target_outputs, output_fl): 
+
+    import xlsxwriter
+
+    list_name = ["first sheet", "second sheet", "third sheet"]
+
+    workbook = xlsxwriter.Workbook(output_fl)
+    col_format = workbook.add_format({'bold': True})
+
+    for category in target_outputs: 
+
+        model_inits, results = target_outputs[category]
+
+        single_model_groups = None
+        
+        if "TM" in category: single_model_groups = single_model_groups_TM
+        elif "QS" in category: single_model_groups = single_model_groups_QS
+        else: assert False
+
+        group_mappings = json.load(open(GROUP_MAPPINGS,'r'))
+        group_mappings_new = {}
+
+        for id in group_mappings:
+            if group_mappings[id] in single_model_groups: group_mappings_new[id] = group_mappings[id]
+        
+        group_mappings = group_mappings_new 
+
+        group_order = list(k for k in group_mappings.keys())
+
+        for model in model_inits: 
+            group_mappings[model[0][:-5]] = model[0][:-5].replace("_","-")
+            group_order = [model[0][:-5]] + group_order
+
+        for metric in ["CASP Loss", "NDCG@3"]: 
+
+            worksheet = workbook.add_worksheet(category + " - " + metric)
+
+            for i, group in enumerate(group_order): worksheet.write(0, i + 1, group_mappings[group], col_format)
+
+            for i, target in enumerate(results): 
+
+                target_id = target[:-4]
+
+                worksheet.write(i + 1, 0, target_id, col_format)
+                
+                for k, id in enumerate(group_order): 
+                    group_metric = results[target][id][metric]
+
+                    if group_metric is not None: worksheet.write(i + 1, k + 1, group_metric)
+                    else: worksheet.write(i + 1, k + 1, "N/A")
+
+            # print(results)
+
+    workbook.close()
+    
 ###################################
